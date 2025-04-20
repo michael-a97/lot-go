7dcf45d2db7e8bbab71363b19ec2c4c266d7cb92
Michael Asnake <105238111+michael-dukka@users.noreply.github.com>
2025-04-20 20:00:02 +0300

    fix: Move firebase token from request body to header for sign up endpoint

--- api/dto/user.go
+++ api/dto/user.go
@@ -1,6 +1,7 @@
 package dto
 
 type UserDto struct {
+	ID          uint   `json:"id"`
 	PhoneNumber string `json:"phone_number" gorm:"unique"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
